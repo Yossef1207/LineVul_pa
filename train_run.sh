4f74f04ae43a5df7f8dfcdbb3240acdf38d8b5e7
Yossef Al Buni <czt0517@hpc3.hpc.tuhh.de>
2025-12-21 23:40:41 +0100

    trained with codellama & gpt-40

--- train_run.sh
+++ train_run.sh
@@ -1,21 +1,43 @@
-#!/bin/bash
+#!/bin/bash -l
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yossef.albuni@tuhh.de
+#SBATCH --time 6-23:00:00
+
+#SBATCH --gres gpu:1
+#SBATCH --mem-per-gpu 75000
+#SBATCH --output output/training_test.log
+
+# Load anaconda und aktiviere das linevul-Env korrekt im Batch-Skript
+module load anaconda/2023.07-1
+eval "$(conda shell.bash hook)"
+conda activate linevul
+
+nproc_per_node=1
+llms=(codellama gpt-4o)
 
 cd linevul
-python linevul_main.py \
-  --output_dir=./saved_models \
-  --model_type=roberta \
-  --tokenizer_name=microsoft/codebert-base \
-  --model_name_or_path=microsoft/codebert-base \
-  --do_train \
-  --do_test \
-  --train_data_file=../data/reposvul_dataset/train.csv \
-  --eval_data_file=../data/reposvul_dataset/val.csv \
-  --test_data_file=../data/reposvul_dataset/test.csv \
-  --epochs 10 \
-  --block_size 512 \
-  --train_batch_size 16 \
-  --eval_batch_size 16 \
-  --learning_rate 2e-5 \
-  --max_grad_norm 1.0 \
-  --evaluate_during_training \
-  --seed 123456  2>&1 | tee train.log
+
+for llm in "${llms[@]}"; do
+  echo ">>> Training mit $llm"
+
+  python linevul_main.py \
+    --output_dir=./saved_models_with_"$llm" \
+    --model_type=roberta \
+    --tokenizer_name=microsoft/codebert-base \
+    --model_name_or_path=microsoft/codebert-base \
+    --do_train \
+    --do_test \
+    --train_data_file=../data/reposvul_with_"$llm"/train_aug.csv \
+    --eval_data_file=../data/reposvul_dataset/val.csv \
+    --test_data_file=../data/reposvul_dataset/test.csv \
+    --epochs 10 \
+    --block_size 512 \
+    --train_batch_size 16 \
+    --eval_batch_size 16 \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --seed 123456  2>&1 | tee "train_with_${llm}.log"
+done
